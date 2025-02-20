b76d2b97fb0445110dd9c38e3db96eed8b720764
princeghiniya <princeg@unityinfoway.com>
2025-02-20 10:35:22 +0530

    Code upload

---
+++ config/middlewares.js
@@ -0,0 +1,41 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'X-Custom-Header',
+      ],
+      credentials: true,
+      maxAge: 3600,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+
